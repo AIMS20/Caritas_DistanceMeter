9b84932608a6dd687abe59e90976d36200da5645
AIMS20 <58343132+AIMS20@users.noreply.github.com>
2020-12-14 23:26:00 +0100

    added calculation of filllevel-percentage based on height

--- SR04/SR04_UltrasonicSensor/SR04_UltrasonicSensor.ino
+++ SR04/SR04_UltrasonicSensor/SR04_UltrasonicSensor.ino
@@ -1,9 +1,14 @@
 #include <HCSR04.h>
 
 // Initialize sensor that uses digital pins 13 and 12.
-int triggerPin = 7;
-int echoPin = 8;
+const int triggerPin = 7;
+const int echoPin = 8;
 UltraSonicDistanceSensor distanceSensor(triggerPin, echoPin);
+
+float distance;
+float fillLevel;
+float mountingHeight = 130;
+int echoCount = 10;
 
 void setup () {
     Serial.begin(9600);  // We initialize serial connection so that we could print values from sensor.
@@ -11,7 +16,21 @@ void setup () {
 
 void loop () {
     // Every 500 miliseconds, do a measurement using the sensor and print the distance in centimeters.
-    double distance = distanceSensor.measureDistanceCm();
-    Serial.println(distance);
-    delay(500);
+        
+            distance = distanceSensor.measureDistanceCm();
+
+        
+                
+
+        fillLevel = getPercentage(distance, mountingHeight);
+        Serial.print("%: ");
+        Serial.println(fillLevel);
+}
+
+//calculate fill-percentage depending on mounting-height of sensor (!)
+float getPercentage(float distance, float mountingHeight){
+    float d = distance;
+    float m = mountingHeight;
+    float fillHeight = m-d;
+    return (fillHeight/m*100);
 }
