20cdc372dd6d59b8f2195a1b206bb6f16fabc252
AIMS20 <58343132+AIMS20@users.noreply.github.com>
2021-01-07 19:17:25 +0100

    updated for esp32:
    -Replaced magic number with pauseMeasurement
    -Updated baud-rate
    -Added pinMode
    -Added debug output

--- SR04/SR04_UltrasonicSensor/SR04_UltrasonicSensor.ino
+++ SR04/SR04_UltrasonicSensor/SR04_UltrasonicSensor.ino
@@ -1,27 +1,34 @@
 #include <HCSR04.h>
 #include "QuickMedianLib.h"
 
+
 // Initialize sensor that uses digital pins
-const int triggerPin = 7;
-const int echoPin = 8;
+const int triggerPin = 18;
+const int echoPin = 19;
 UltraSonicDistanceSensor distanceSensor(triggerPin, echoPin);
 
 const float mountingHeight = 130;   //in cm
 const int echoCount = 10;           //how often measurement will be taken before going back to sleep
+const int pauseMeasurement = 500;    //in miliseconds
 float distanceVals[echoCount];
 float distance;                     //in cm
 float fillLevel;                    //in percent
 
 void setup () {
- Serial.begin(9600);  // We initialize serial connection so that we could print values from sensor
+ Serial.begin(115200);  // We initialize serial connection so that we could print values from sensor
 
+Serial.println("Setting Pins...");
+  // Set SR04 pins
+  pinMode(triggerPin, OUTPUT);
+  pinMode(echoPin, INPUT);
 }
 
 void loop () {
     // Every 500 miliseconds, do a measurement using the sensor and print the distance in centimeters
         
         //get array of multiple distance-levels to calc median afterwards: prunes out false readings 
-        getDistanceVals(echoCount);
+Serial.println("Starting Loop...");
+        getDistanceVals(echoCount, pauseMeasurement);
         
         //calculate Median of distancevals
         int dValsLength = sizeof(distanceVals) / sizeof(distanceVals[0]); 
@@ -35,6 +42,7 @@ void loop () {
         printLevel(fillLevel);
 
 
+Serial.println("Ending Loop...");
         delay(5000); //TODO: Replace with deep sleep
 }
 
@@ -43,7 +51,8 @@ float getPercentage(float distance, float mountingHeight){
     return (fillHeight/mountingHeight*100);
 }
 
-void getDistanceVals(int echoCount){
+void getDistanceVals(int echoCount, int pauseMeasurement){
+Serial.println("in Method...");
     float distance;
     // float distanceVals[echoCount];
     for (int i = 0; i < echoCount;){
@@ -53,13 +62,18 @@ void getDistanceVals(int echoCount){
             Serial.println(distance);
             i++;
         }
-        delay(500); //x echoCount amounts to 5s
+        else
+        {
+            Serial.println("FUCKY WUCKY");
+        }
+        
+        delay(pauseMeasurement);
     }
 }
 
 void printLevel(float fillLevel){
     if (fillLevel <= 0 || fillLevel > 100){
-        Serial.print("ERROR!");
+        Serial.print("ERROR! value too big/small");
         return;
     }
 
